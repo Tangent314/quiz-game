007c15a5f3a6e6877cfdc1d0b2bed6cbb467b8a0
Tangent314 <blairmclaughlin@Blairs-MacBook-Pro.local>
2019-09-04 22:05:51 -0500

    add functionality to buildQuiz()

--- game.js
+++ game.js
@@ -2,7 +2,41 @@ const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
-function buildQuiz(){}
+function buildQuiz(){
+    // we'll need a place to store HTML output
+    const output = [];
+
+    // for each question...
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+
+            // we'll want to score the list of answer choices
+            const answers = [];
+
+            // and for each available answer...
+            for(letter in currentQuestion.answers){
+
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
+                    <input type="radio" name="question${questionNumber}" value="${letter}">
+                    ${letter} :
+                    ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>`
+            );
+        }
+    );
+
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
+}
 
 function showResults(){}
 
