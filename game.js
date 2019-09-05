c878518bbdf59cc7272cec856530dc8eb11a5868
Tangent314 <blairmclaughlin@Blairs-MacBook-Pro.local>
2019-09-04 21:34:00 -0500

    create questions

--- game.js
+++ game.js
@@ -11,3 +11,33 @@ buildQuiz();
 
 //on submit, show results
 submitButton.addEventListener('click', showResults);
+
+const myQuestions = [
+    {
+        question: "Who is the strongest?",
+        answers: {
+            a: "Superman",
+            b: "The Terminator";
+            c: "Waluigi, duh"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Where is Waldo really?",
+        answers: {
+            a: "Spain",
+            b: "Japan",
+            c: "USA"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What's my cat's name?",
+        answers: {
+            a: "Chai",
+            b: "Fluffy",
+            c: "Randy"
+        },
+        correctAnswer: "a"
+    }
+]
